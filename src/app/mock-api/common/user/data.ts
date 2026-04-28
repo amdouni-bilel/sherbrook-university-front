7d94b8ff8fb6971b43a65408e11edd8fd97cba96
amdouni-bilel <amdounibilel1@gmail.com>
2026-04-28 14:05:18 +0100

    first commit

---
+++ src/app/mock-api/common/user/data.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export const user = {
+    id: 'cfaad35d-07a3-4447-a6c3-d8c3d54fd5df',
+    name: 'Amdouni Bilel',
+    email: 'amdouni.bilel@company.com',
+    avatar: 'images/avatars/poulina.jpg',
+    status: 'online',
+};
---
+++ src/app/modules/ipfp/components/tasks/list-tasks/list-tasks.component.ts
@@ -0,0 +1,13 @@
+// list-tasks.component.ts
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-list-tasks',
+    standalone: true,
+    imports: [],
+    templateUrl: './list-tasks.component.html',
+    styleUrl: './list-tasks.component.scss'
+})
+export class ListTasksComponent {
+
+}
